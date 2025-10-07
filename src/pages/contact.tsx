2f2378b0c65b5086460457f0d27221cb2c4fb6df
thealexzheng <202692763+thealexzheng@users.noreply.github.com>
2025-10-06 20:34:41 -0500

    mo' better
    syntax and l2comment

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -9,12 +9,14 @@ const IndexPage: React.FC<PageProps> = () => {
       <p>
         Alex is ready and excited to help you with all your real estate needs. Call, text, or email: I look forward to hearing from you!
       </p>
-      <p>
-        <strong>Phone:</strong> <a href="tel:+12818868168">(281) 886-8168</a>
-      </p>
-      <p>
-        <strong>Email:</strong> <a href="mailto:contact+alexzhengrealtor@alexzhengrealtor.com">contact@alexzhengrealtor.com</a>
-      </p>
+      <address>
+        <p>
+          <strong>Phone:</strong> <a href="tel:+12818868168">(281) 886-8168</a>
+        </p>
+        <p>
+          <strong>Email:</strong> <a href="mailto:contact+alexzhengrealtor@alexzhengrealtor.com">contact@alexzhengrealtor.com</a>
+        </p>
+      </address>
     </Layout>
     </>
   )
